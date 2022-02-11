1b3a062e3fa11c570432c58214cadd19a20adad8
AlphaCoder04 <alex04gushawin04@gmail.com>
2022-02-11 22:18:02 +0100

    Enemy
    Der Enemy (Frau Karck) kann sich entlang Waypoints zum Zentrum bewegen

--- TD-Survival/TD Survival Unity/Assets/Scripts/Enemy.cs
+++ TD-Survival/TD Survival Unity/Assets/Scripts/Enemy.cs
@@ -2,17 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+public class Enemy : MonoBehaviour{
+
+    public float speed = 5;
+    private Waypoint Wpoints;
+
+    private int waypointIndex = 0;
+    public int hp = 100;
+
+    void Start(){
         
+        Wpoints = GameObject.FindGameObjectWithTag("Waypoint").GetComponent<Waypoint>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+
+        transform.position = Vector2.MoveTowards(transform.position, Wpoints.waypoints[waypointIndex].position, speed * Time.deltaTime);
+
+        if(Vector2.Distance(transform.position, Wpoints.waypoints[waypointIndex].position) < 0.1f){
+            if(waypointIndex< Wpoints.waypoints.Length -1){
+                waypointIndex++;
+            }
+            else{
+                Destroy(gameObject);
+            }
+        }
+
+        if(hp <= 0){
+            Destroy(gameObject);
+        }
     }
 }
