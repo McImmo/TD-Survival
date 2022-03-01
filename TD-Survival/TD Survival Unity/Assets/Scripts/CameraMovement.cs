9ec6bb1140b715f6fa818993f1280bce6a5da333
NinaLS <nina@ka-ja.org>
2022-03-01 22:09:18 +0100

    Camera Movement optimiert

--- TD-Survival/TD Survival Unity/Assets/Scripts/CameraMovement.cs
+++ TD-Survival/TD Survival Unity/Assets/Scripts/CameraMovement.cs
@@ -6,38 +6,44 @@ public class CameraMovement : MonoBehaviour
 {
     public Transform player;
     public Vector3 playerP;
-    public int boundary = 50;
+    public float boundary = 0.45f;
+    float height;
+    float width;
+    
     // Start is called before the first frame update
     void Start()
     {
         
         transform.position = player.position;
+        width = GetComponent<Camera>().pixelWidth * boundary;
+        height = GetComponent<Camera>().pixelHeight * boundary;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
         Vector3 move = new Vector3(0,0,0);
         playerP = GetComponent<Camera>().WorldToScreenPoint(player.position);
-        if(playerP.y < boundary) 
+        if(playerP.y < height) 
         {
-            move.y = move.y + (playerP.y - boundary);
+            move.y = move.y + (playerP.y - height);
         }   
-        if(playerP.y > (GetComponent<Camera>().pixelHeight - boundary)) 
+        if(playerP.y > (GetComponent<Camera>().pixelHeight - height)) 
         {
-            move.y = move.y + (playerP.y - (GetComponent<Camera>().pixelHeight - boundary));
+            move.y = move.y + (playerP.y - (GetComponent<Camera>().pixelHeight - height));
         }
-        if(playerP.x < boundary)
+        if(playerP.x < width)
         {
-            move.x = move.x + (playerP.x - boundary);
+            move.x = move.x + (playerP.x - width);
         }
-        if(playerP.x > (GetComponent<Camera>().pixelWidth - boundary)) 
+        if(playerP.x > GetComponent<Camera>().pixelWidth - width)
         {
-            move.x = move.x + (playerP.x - (GetComponent<Camera>().pixelWidth - boundary));
+            move.x = move.x + (playerP.x - (GetComponent<Camera>().pixelWidth - width));
         }
         Vector3 cameraP = GetComponent<Camera>().WorldToScreenPoint(transform.position);
         cameraP = cameraP + move;
         transform.position = GetComponent<Camera>().ScreenToWorldPoint(cameraP);
+        
     }
 }
