a63068028343e35fbe18144cef2cb77d78837225
NinaLS <nina@ka-ja.org>
2022-03-03 20:18:34 +0100

    resourcen animation und camera movement optimiert wiederhergestellt

--- TD-Survival/TD Survival Unity/Assets/Scripts/PlayerMovement.cs
+++ TD-Survival/TD Survival Unity/Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
     Vector2 movement;
     public Animator animator;
+    bool bautAb = false;
 
     // Update is called once per frame
     void Update()
@@ -19,11 +20,22 @@ public class PlayerMovement : MonoBehaviour
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
+        animator.SetBool("BautAb", bautAb);
     }
 
     //Wird eine festgelegte Anzahl pro Sekunde aufgerufen
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime); //Player wird bewegt
+    }
+
+    public bool GetBautAb()
+    {
+        return bautAb;
+    }
+
+    public void SetBautAb(bool set)
+    {
+        bautAb = set;
     }
 }
