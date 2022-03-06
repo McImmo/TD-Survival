f9d2477069023ee60825c9cc193765e0eba40a5f
AlphaCoder04 <alex04gushawin04@gmail.com>
2022-03-06 21:59:36 +0100

    v.2

--- TD-Survival/TD Survival Unity/Assets/Scripts/Inventory/Item.cs
+++ TD-Survival/TD Survival Unity/Assets/Scripts/Inventory/Item.cs
@@ -11,4 +11,12 @@ public class Item
 
     public ItemType itemType;
     public int amount;
+
+    public Sprite GetSprite(){
+        switch (itemType) {
+            default:
+            case ItemType.Wood:  return ItemAssets.Instance.woodSprite;
+            case ItemType.Stone: return ItemAssets.Instance.stoneSprite;
+        }
+    }
 }
