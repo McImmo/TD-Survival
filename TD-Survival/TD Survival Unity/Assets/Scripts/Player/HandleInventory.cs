48401257c9e187d5e21eee463d5ed52106856a94
AlphaCoder04 <alex04gushawin04@gmail.com>
2022-03-06 20:38:11 +0100

    Inventory
    Backup

---
+++ TD-Survival/TD Survival Unity/Assets/Scripts/Player/HandleInventory.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleInventory : MonoBehaviour
+{
+    private Inventory inventory;
+
+    [SerializeField] private UI_Inventory uiInventory;
+
+    public void Awake(){
+        inventory = new Inventory();
+        uiInventory.SetInventory(inventory);
+    }
+}
