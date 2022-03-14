4aa91bb3cc9ce79adbbe7c3d2e047aff2bf56dfc
NinaLS <nina@ka-ja.org>
2022-03-14 19:47:42 +0100

    Day Night Cycle version 1

---
+++ TD-Survival/TD Survival Unity/Assets/Scripts/DayNightCycle.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class DayNightCycle : MonoBehaviour
+{
+    [SerializeField] private Gradient lightColor;
+
+    private int nights;
+    public int Nights => nights;
+    public float time = 0;
+    public int cycleTime = 1000;
+    private bool isNight = false;
+    public bool GetIsNight => isNight;
+
+    void Update()
+    {
+        if(time > cycleTime)
+        {
+            time = 0;
+            nights++;
+        }
+
+        if((int)time == cycleTime/2) isNight = true;
+
+        if(time > cycleTime*0.95f) isNight = false;
+        
+        time += Time.deltaTime;
+        GetComponent<Light2D>().color = lightColor.Evaluate(time * 1/cycleTime);
+    }
+}
--- TD-Survival/TD Survival Unity/Assets/Scripts/Enemy/Spawner.cs
+++ TD-Survival/TD Survival Unity/Assets/Scripts/Enemy/Spawner.cs
@@ -9,14 +9,18 @@ public class Spawner : MonoBehaviour
     private float spawnCooldown = 1;
     [SerializeField]
     private GameObject EnemyPrefab;
+    [SerializeField] private DayNightCycle light;
 
     private bool TimeToSpawn(){
         return Time.time >= nextSpawnTime;
     }
 
     private void Spawn(){
-        nextSpawnTime = Time.time + spawnCooldown;
-        GameObject Enemy= Instantiate(EnemyPrefab, transform.position, transform.rotation);
+        if(light.GetIsNight)
+        {
+            nextSpawnTime = Time.time + spawnCooldown;
+            GameObject Enemy= Instantiate(EnemyPrefab, transform.position, transform.rotation);
+        }
     }
     void Update()
     {
