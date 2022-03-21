7be341561e17c91bb73a893ffd094937a13b9805
NinaLS <nina@ka-ja.org>
2022-03-21 14:18:35 +0100

    Rsourcen am anfang

--- TD-Survival/TD Survival Unity/Assets/Scripts/resourcen/ResourcenSpawner.cs
+++ TD-Survival/TD Survival Unity/Assets/Scripts/resourcen/ResourcenSpawner.cs
@@ -14,24 +14,30 @@ public class ResourcenSpawner : MonoBehaviour
     private int xMax = 100;
     private int xMin = -85;
     private bool abgebaut = false;
+    [SerializeField] private int anfangsMenge;
+
+    private void Start()
+    {
+        for(int i = 0; i < anfangsMenge; i++)
+        {
+            Spawn(130,-110,95,-73);
+        }
+    }
 
     private bool TimeToSpawn(){
         return Time.time >= nextSpawnTime;
     }
 
-    private void Spawn(){
-            if(abgebaut)
-            {            
-                nextSpawnTime = Time.time + spawnCooldown;
-                Vector2 pos = new Vector2 (Random.Range (xMin, xMax), Random.Range (yMin, yMax));
-                GameObject resource = Instantiate(resourcePrefab, pos, transform.rotation);
-                abgebaut = false;
-            }
+    private void Spawn(int xMa, int xMi, int yMa, int yMi){          
+        nextSpawnTime = Time.time + spawnCooldown;
+        Vector2 pos = new Vector2 (Random.Range (xMi, xMa), Random.Range (yMi, yMa));
+        GameObject resource = Instantiate(resourcePrefab, pos, transform.rotation);
+        abgebaut = false;
     }
     void Update()
     {
-                if(TimeToSpawn()){
-            Spawn();
+        if(TimeToSpawn() && abgebaut){
+            Spawn(xMax, xMin, yMax, yMin);
         }
     }
 
