b9a5d2a5501dc29ec962193dc71b6631e22bc28b
McImmo <immanuel.koroneos@gmail.com>
2022-03-22 01:21:31 +0100

    shotgun tower
    kostet 4 stein

--- TD-Survival/TD Survival Unity/Assets/Scripts/Tower/BarrelRotation.cs
+++ TD-Survival/TD Survival Unity/Assets/Scripts/Tower/BarrelRotation.cs
@@ -7,17 +7,20 @@ public class BarrelRotation : MonoBehaviour
     public Transform pivot;
     public Transform barrel;
 
-    public Tower tower;
+    public ShotgunTower tower;
     void Update()
     {
         if(tower != null){
+            
             if(tower.currentTarget != null) {
+                
                 Vector2 relative = tower.currentTarget.transform.position - pivot.position; //Vektor von pivot zu ziel
         
                 float angle = Mathf.Atan2(relative.y, relative.x) * Mathf.Rad2Deg; //Winkel in Rad
 
                 Vector3 newRotation = new Vector3(0,0, angle);
-
+                Debug.Log("daite " + newRotation.z);
+                
                 pivot.localRotation = Quaternion.Euler(newRotation);
             }
         }
---
+++ TD-Survival/TD Survival Unity/Assets/Scripts/Tower/ShotgunTower.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotgunTower : MonoBehaviour
+{
+[SerializeField]private float range = 20f;
+[SerializeField]private float timeBetweenShots = 1f; // in seconds
+private float nextTimeToShoot;
+public GameObject currentTarget;
+public Transform pivot;
+public Transform barrel;
+public GameObject projectile;
+
+private void Start()
+{
+   nextTimeToShoot = Time.time;
+}
+
+private void updateNearestEnemy()
+{
+    GameObject currentNearestEnemy = null;
+    float distance = Mathf.Infinity;
+
+    foreach(GameObject enemy in EnemiesInfo.enemies){
+        if(enemy != null)
+        {
+        float _distance = (transform.position - enemy.transform.position).magnitude;
+
+        if (_distance < distance){
+            distance = _distance;
+            currentNearestEnemy = enemy;
+
+        }
+        }
+    }
+
+    if (distance <= range){
+        currentTarget = currentNearestEnemy;
+    }
+    else{
+        currentTarget = null;
+    }
+}
+
+private void shoot(){
+        
+    Enemy enemyScript = currentTarget.GetComponent<Enemy>();
+    GameObject newProjectile1 = Instantiate(projectile, barrel.position, pivot.rotation);
+    GameObject newProjectile2 = Instantiate(projectile, barrel.position, (pivot.rotation * Quaternion.Euler(0, 0, 45)));
+    GameObject newProjectile3 = Instantiate(projectile, barrel.position, (pivot.rotation * Quaternion.Euler(0, 0, -45)));
+
+
+    //enemyScript.TakeDamage(damage); wird im Moment durch das Projektil gehandled wenn es den gegner beruehrt
+        
+}
+
+
+private void Update()
+{
+    updateNearestEnemy();
+
+    if(Time.time >= nextTimeToShoot){
+        if (currentTarget != null){
+            shoot();
+            nextTimeToShoot = Time.time + timeBetweenShots;
+        }
+    }
+
+}
+}
